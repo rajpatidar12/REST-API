c5560307c3f30957c794ace7099314fdcc970a50
rajpatidar12 <patidarraj465@gmail.com>
2024-11-23 22:14:57 +0530

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false })); // For x-www-form-urlencoded
+app.use(express.json()); // For application/json
+
 app.get("/users", (req, res) => {
   const html = `
   <ul>
@@ -13,10 +19,26 @@ app.get("/users", (req, res) => {
 app.get("/api/users", (req, res) => {
   return res.json(users);
 });
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id);
-  return res.json(user);
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    return res.json({ status: "pending" });
+  });
+// Routes
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success", id: users.length });
+  });
 });
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
